dc0b96b7878420c2323e654b1883641b4f46870e
Aaron Patterson <tenderlove@ruby-lang.org>
2025-12-08 12:26:17 -0600

    generating properties

--- gen.rb
+++ gen.rb
@@ -4,6 +4,51 @@ require "uri"
 require "fileutils"
 require "erb"
 
+handler = ERB.new(<<-eot, trim_mode: '-')
+def handle io, id, flags
+<%- types.each_with_index do |type, i| -%>
+  <%= i == 0 ? "if" : "elseif" %> id == <%= sprintf("%#04x", type.value) %> then
+  <%- if type.flags -%>
+    raise "wrong flags" unless flags == <%= sprintf("%#04x", type.flags) %>
+  <%- end -%>
+    handle_<%= type.name.downcase %>(io, flags)
+<%- end -%>
+  else
+    raise "unknown id \#{id}"
+  end
+end
+eot
+
+class PropertyTemplate
+  LUT = {
+    "Byte" => "io.getbyte",
+    "Two Byte Integer" => "io.getbyte << 8 | io.getbyte",
+    "Four Byte Integer" => "io.getbyte << 24 | io.getbyte << 16 | io.getbyte << 8 | io.getbyte",
+    "UTF-8 Encoded String" => "io.read(io.getbyte << 8 | io.getbyte).force_encoding('UTF-8')",
+    "UTF-8 String Pair" => "[io.read(io.getbyte << 8 | io.getbyte).force_encoding('UTF-8'), io.read(io.getbyte << 8 | io.getbyte).force_encoding('UTF-8')]",
+    "Binary Data" => "io.read(io.getbyte << 8 | io.getbyte)",
+    "Variable Byte Integer" => "read_varint(io)",
+  }
+  TEMPLATE = ERB.new(<<-eot, trim_mode: '-')
+<%- props.each do |type, values| -%>
+def <%= type.downcase.gsub(/ /, '_') %><%= type =~ /properties/i ? "" : "_properties" %> io, id
+  <%- values.each_with_index do |val, i| -%>
+  <%= i == 0 ? "if" : "elseif" %> id == <%= sprintf("%#04x", val.ident) %> then # <%= val.name %>
+    <%= LUT.fetch(val.type) %>
+  <%- end -%>
+  else
+    raise "wrong property \#{id}"
+  end
+end
+
+<%- end -%>
+eot
+
+  def self.result props
+    TEMPLATE.result binding
+  end
+end
+
 cache_dir = ENV["XDG_CACHE_HOME"] || File.join(ENV["HOME"], ".cache", "mqttlo")
 FileUtils.mkdir_p cache_dir
 cache_file = File.join(cache_dir, "mqtt.dump")
@@ -16,17 +61,22 @@ if File.exist?(cache_file)
 end
 
 if etag
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  req = Net::HTTP::Get.new(uri.request_uri)
-  req["If-None-Match"] = etag
-  response = http.request(req)
+  stat = File.stat(cache_file)
+  if (Time.now - stat.mtime) > (2 * 365 * 24 * 60 * 60)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req["If-None-Match"] = etag
+    response = http.request(req)
 
-  if response.code != "304"
-    # update cache
-    etag = response["ETag"]
-    File.binwrite(cache_file, Marshal.dump([etag, response.body]))
-    etag, response_body = Marshal.load(File.binread(cache_file))
+    if response.code != "304"
+      # update cache
+      etag = response["ETag"]
+      File.binwrite(cache_file, Marshal.dump([etag, response.body]))
+      etag, response_body = Marshal.load(File.binread(cache_file))
+    else
+      File.utime(stat.atime, Time.now, cache_file)
+    end
   end
 else
   # no cache
@@ -39,10 +89,12 @@ end
 doc = Nokogiri::HTML.parse response_body
 
 PacketType = Struct.new(:name, :value, :description, :flags)
-Property = Struct.new(:ident, :name, :type, :packet)
+Property = Struct.new(:ident, :name, :type, :packets)
 Reason = Struct.new(:name, :value, :description)
 
 def packet_types doc
+  fbs = flag_bits(doc).to_h
+
   node = doc.css("a[name='_Table_2.1_-']").first.parent
   while node.name != "table"
     node = node.next_sibling
@@ -50,7 +102,7 @@ def packet_types doc
   node.css("tr").map { |tr|
     tr.css("td").map { |x| x.text.strip }
   }.select { |name, | name =~ /^[A-Z]*$/ }.map { |name, value, _, desc|
-    PacketType.new(name, value.to_i, desc, 0)
+    PacketType.new(name, value.to_i, desc, fbs[name])
   }
 end
 
@@ -62,7 +114,7 @@ def properties doc
   node.css("tr").map { |tr|
     tr.css("td").map { |x| x.text.strip }
   }.select { |dec, | dec =~ /^[0-9]*$/ }.map { |dec, _, name, type, packet|
-    Property.new(dec.to_i, name, type, packet)
+    Property.new(dec.to_i, name, type, packet.split(",").map(&:strip))
   }
 end
 
@@ -78,16 +130,30 @@ def reasons doc
   }
 end
 
+def flag_bits doc
+  node = doc.css("a[name='_Table_2.2_-']").first.parent
+  while node.name != "table"
+    node = node.next_sibling
+  end
+  node.css("tr").map { |tr|
+    tr.css("td").map { |x| x.text.strip }
+  }.select { |dec, *rest|
+    dec =~ /^[A-Z]*$/ && rest.last =~ /^[01]$/
+  }.map { |type, _, bit3, bit2, bit1, bit0|
+    [type, bit3.to_i << 3 | bit2.to_i << 2 | bit1.to_i << 1 | bit0.to_i]
+  }
+end
+
+def print_props props, handler
+  puts handler.result(props)
+end
+
 types = packet_types doc
 props = properties doc
 rs = reasons(doc)
+properties_by_type = Hash.new { |h,k| h[k] = [] }
+props.each { |p| p.packets.each { |type| properties_by_type[type] << p } }
+p properties_by_type.keys
+print_props(properties_by_type, PropertyTemplate)
 
-template = ERB.new(DATA.read)
-types.each do |type|
-  p type
-end
-
-__END__
-
-def handle_packet id, flags
-end
+#puts handler.result(binding)
